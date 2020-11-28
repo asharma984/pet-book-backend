9057178a943180775afbe3e0c496920265595c4a
asharma984 <aditya.sharma984@gmail.com>
2020-11-28 02:01:27 -0500

    added mongodb connection and user model

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express=require("express");
 const mongoose=require("mongoose");
 const cors=require("cors");
 
-require("dotenv").config;
+require("dotenv").config();
 
-
+//set up express
 const app=express();
 app.use(express.json());
 app.use(cors());
@@ -12,3 +12,14 @@ app.use(cors());
 const PORT=process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server started on port: ${PORT}`));
+
+//setup mongoose
+const uri=process.env.MONGODB_CONNECTION_STRING;
+mongoose.connect(uri, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true}
+    ,(err)=>{
+        if(err) throw err;
+        console.log("Mongodb connection established successfully")
+       }
+    )
