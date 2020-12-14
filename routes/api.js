755373820a5d8494ea12466832d18c9319a2959c
David Ryan <ryan.davi@husky.neu.edu>
2020-12-13 20:01:35 -0500

    added find pet by id

--- routes/api.js
+++ routes/api.js
@@ -81,6 +81,28 @@ router.route('/petfinder/animals/&').get((req, res) => {
         });
 });
 
+// get an animal by ID
+router.route('/petfinder/:animalId').get((req, res) => {
+    fetch(`${PetFinderAuthURL}`, {
+    method: 'POST',
+    body: `${COMPRISED}`,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+})
+    .then(response => response.json())
+        .then(responsejson => {
+              fetch(`https://api.petfinder.com/v2/animals/${req.params.animalId}`, {
+                  headers: {
+                      'Authorization': responsejson.token_type + ' '
+                                       + responsejson.access_token,
+                      'Content-Type': 'application/x-www-form-urlencoded'
+                  }
+              })
+                  .then(response => response.json())
+                .then(pet => res.json(pet))
+        })
+})
 
 // get cities with population over 5,000 for a specific state
 router.route('/location/cities/:theState').get((req, res) => {
@@ -93,7 +115,6 @@ router.route('/location/cities/:theState').get((req, res) => {
         }
     }).then(response => response.json())
 
-        //get animal types
         .then(response => {
             fetch(`https://www.universal-tutorial.com/api/cities/${req.params.theState}`, {
                 headers: {
